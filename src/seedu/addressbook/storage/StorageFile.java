b9e3bac72a12713f77abcb22963707df211f308c
0WN463 <E0175456@u.nus.edu>
2018-09-13 22:34:17 +0800

    DIP completed

--- src/seedu/addressbook/storage/StorageFile.java
+++ src/seedu/addressbook/storage/StorageFile.java
@@ -15,7 +15,7 @@ import java.nio.file.Paths;
 /**
  * Represents the file used to store address book data.
  */
-public class StorageFile {
+public class StorageFile extends Storage{
 
     /** Default file path used if the user doesn't provide the file name. */
     public static final String DEFAULT_STORAGE_FILEPATH = "addressbook.txt";
@@ -32,17 +32,7 @@ public class StorageFile {
             super(message);
         }
     }
-
-    /**
-     * Signals that some error has occured while trying to convert and read/write data between the application
-     * and the storage file.
-     */
-    public static class StorageOperationException extends Exception {
-        public StorageOperationException(String message) {
-            super(message);
-        }
-    }
-
+    
     private final JAXBContext jaxbContext;
 
     public final Path path;
