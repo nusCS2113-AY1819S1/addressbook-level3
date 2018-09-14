1c8897a81dc676e31a14b3c8f9019a32cd058fcd
0WN463 <E0175456@u.nus.edu>
2018-09-14 12:51:19 +0800

    Remove unrelated files

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -39,10 +39,12 @@ public abstract class Command {
     /**
      * Executes the command and returns the result.
      */
+    public CommandResult execute(){
+        throw new UnsupportedOperationException("This method should be implement in child classes");
+    }
 
     //Note: it is better to make the execute() method abstract, by replacing the above method with the line below:
     //public abstract CommandResult execute();
-    public abstract CommandResult execute();
 
     /**
      * Supplies the data the command will operate on.
@@ -68,5 +70,4 @@ public abstract class Command {
     public void setTargetIndex(int targetIndex) {
         this.targetIndex = targetIndex;
     }
-
 }
