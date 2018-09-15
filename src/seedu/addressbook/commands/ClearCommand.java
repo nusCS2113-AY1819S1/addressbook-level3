466400c09222199006448a92d91cff471502eb48
Tan Wei Ming <e0176337@u.nus.edu>
2018-09-15 11:56:14 +0800

    execute() in Command class is now abstract

--- src/seedu/addressbook/commands/ClearCommand.java
+++ src/seedu/addressbook/commands/ClearCommand.java
@@ -11,7 +11,6 @@ public class ClearCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
 
-    @Override
     public CommandResult execute() {
         addressBook.clear();
         return new CommandResult(MESSAGE_SUCCESS);
