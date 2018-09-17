eb8792020782639c67279568feb302137a09262d
jamesyaputra <jamesyaputra1704@gmail.com>
2018-09-17 10:08:45 +0800

    Added Printable interface and made all person data point implement it

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -6,7 +6,7 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Represents a Person's address in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
  */
-public class Address {
+public class Address implements Printable {
 
     public static final String EXAMPLE = "123, some street";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
@@ -50,6 +50,12 @@ public class Address {
     @Override
     public int hashCode() {
         return value.hashCode();
+    }
+
+    @Override
+    public java.lang.String getPrintableString() {
+        final String printable = "Address: " + this.value;
+        return printable;
     }
 
     public boolean isPrivate() {
