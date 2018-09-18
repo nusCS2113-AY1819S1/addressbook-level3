ef9e4fab452a1fe72c80346c18550571c6fd91c0
micklwl <micklwl@hotmail.com>
2018-09-18 17:20:59 +0800

    Update main import format

--- src/seedu/addressbook/Main.java
+++ src/seedu/addressbook/Main.java
@@ -2,7 +2,6 @@ package seedu.addressbook;
 
 import javafx.application.Application;
 import javafx.application.Platform;
-
 import javafx.stage.Stage;
 import seedu.addressbook.logic.Logic;
 import seedu.addressbook.ui.Gui;
