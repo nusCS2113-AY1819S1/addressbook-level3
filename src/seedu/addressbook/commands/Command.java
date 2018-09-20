26b298df37091cef90ec2f796fcf138e10908c0d
SarahTaaherBonna <sarahtb25@gmail.com>
2018-09-20 16:41:04 +0800

    Did first part of Polymorphism (i.e. added method boolean isMutating())

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -45,6 +45,7 @@ public abstract class Command {
 
     //Note: it is better to make the execute() method abstract, by replacing the above method with the line below:
     //public abstract CommandResult execute();
+
     public abstract CommandResult execute();
 
     /**
@@ -72,5 +73,8 @@ public abstract class Command {
         this.targetIndex = targetIndex;
     }
 
-    //public boolean isMutating()
+    public boolean isMutating(String userCommandText) {
+        if(userCommandText == "add" || userCommandText == "delete") return true;
+        else return false;
+    }
 }
