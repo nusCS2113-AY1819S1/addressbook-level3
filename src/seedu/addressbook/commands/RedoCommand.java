5143f8c9519e24841dad2d80abda784282e75bbd
Nichpang1 <e0235240@u.nus.edu>
2018-09-22 15:40:44 +0800

    show full list after undo/redo

--- src/seedu/addressbook/commands/RedoCommand.java
+++ src/seedu/addressbook/commands/RedoCommand.java
@@ -1,21 +1,24 @@
 package seedu.addressbook.commands;
 
 import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+import java.util.List;
 
 public class RedoCommand extends Command{
     public static final String COMMAND_WORD = "redo";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" + "Redo previous undid action.\n\t"
             + "Example: " + COMMAND_WORD;
 
-    public static final String MESSAGE_SUCCESS = "Redo successful!";
+    public static final String MESSAGE_SUCCESS = "Redo successful!\n";
     public static final String MESSAGE_FAILURE = "No command to redo";
 
     @Override
     public CommandResult execute() {
         try {
             addressBook.redoLast();
-
-            return new CommandResult(MESSAGE_SUCCESS);
+            List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+            return new CommandResult(MESSAGE_SUCCESS + getMessageForPersonListShownSummary(allPersons), allPersons);
         } catch (AddressBook.HistoryOutOfBoundException hoobe){
             return new CommandResult(MESSAGE_FAILURE);
         }
--- src/seedu/addressbook/commands/UndoCommand.java
+++ src/seedu/addressbook/commands/UndoCommand.java
@@ -1,21 +1,24 @@
 package seedu.addressbook.commands;
 
 import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+import java.util.List;
 
 public class UndoCommand extends Command {
     public static final String COMMAND_WORD = "undo";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" + "Undo previous undo-able action.\n\t"
             + "Example: " + COMMAND_WORD;
 
-    public static final String MESSAGE_SUCCESS = "Undo successful!";
+    public static final String MESSAGE_SUCCESS = "Undo successful!\n";
     public static final String MESSAGE_FAILURE = "No command to undo";
 
     @Override
     public CommandResult execute() {
         try {
             addressBook.undoLast();
-
-            return new CommandResult(MESSAGE_SUCCESS);
+            List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+            return new CommandResult(MESSAGE_SUCCESS + getMessageForPersonListShownSummary(allPersons), allPersons);
         } catch (AddressBook.HistoryOutOfBoundException hoobe){
             return new CommandResult(MESSAGE_FAILURE);
         }
