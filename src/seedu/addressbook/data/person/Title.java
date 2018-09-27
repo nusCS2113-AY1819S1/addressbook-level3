13eb9c4253dca3237b34706c7106f260c77674cc
Nichpang1 <e0235240@u.nus.edu>
2018-09-27 16:39:48 +0800

    Revert "added isPrivate checker in the title class"
    This reverts commit 937376455edbd7d5423418b16f428798abf621ca.

--- src/seedu/addressbook/data/person/Title.java
+++ src/seedu/addressbook/data/person/Title.java
@@ -2,19 +2,14 @@ package seedu.addressbook.data.person;
 
 import seedu.addressbook.data.exception.IllegalValueException;
 
-/**
- * Represents a Person's title in the address book.
- * Guarantees: immutable; is valid as declared in {@link #isValidTitle(String)}
- */
 public class Title {
     public static final String EXAMPLE = "d";
     public static final String MESSAGE_TITLE_CONSTRAINTS = "Title should either 'Doctor' or 'Patient'";
 
     public final String value;
-    private boolean isPrivate;
 
-    public Title(String title, boolean isPrivate) throws IllegalValueException {
-        this.isPrivate = isPrivate;
+
+    public Title(String title) throws IllegalValueException {
         title = title.trim();
         if (!isValidTitle(title)){
             throw new IllegalValueException(MESSAGE_TITLE_CONSTRAINTS);
@@ -43,6 +38,4 @@ public class Title {
     public String toString() {
         return value;
     }
-
-    public boolean isPrivate() { return isPrivate; }
 }
