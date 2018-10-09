d77918008acfaaf662404c8c1854551c18c8c659
SalsabilTasnia <salsabilali70@gmail.com>
2018-10-09 00:15:11 -0700

    Modified the Welcome Message

--- src/seedu/addressbook/common/Messages.java
+++ src/seedu/addressbook/common/Messages.java
@@ -7,8 +7,11 @@ public class Messages {
 
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
+    public static final String MESSAGE_INVALID_MENU_ITEM_DISPLAYED_INDEX = "The menu item index provided is invalid";
     public static final String MESSAGE_PERSON_NOT_IN_ADDRESSBOOK = "Person could not be found in address book";
+    public static final String MESSAGE_MENU_ITEM_NOT_IN_ADDRESSBOOK = "Menu item could not be found in RMS";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
+    public static final String MESSAGE_MENUS_LISTED_OVERVIEW = "%1$d food items listed!";
     public static final String MESSAGE_PROGRAM_LAUNCH_ARGS_USAGE = "Launch command format: " +
             "java seedu.addressbook.Main [STORAGE_FILE_PATH]";
     public static final String MESSAGE_WELCOME = "Welcome to Restaurant Management System! \n" +
@@ -20,4 +23,8 @@ public class Messages {
             "- For further assistance, key in 'help'\n" +
             "----------------------------------------------------------";
     public static final String MESSAGE_USING_STORAGE_FILE = "Using storage file : %1$s";
+
+    public static final String MESSAGE_ORDERS_LISTED_OVERVIEW = "%1$d orders listed!";
+    public static final String MESSAGE_RMS_WELCOME = "Welcome to the Restaurant Management System!";
+    public static final String MESSAGE_USING_ORDER_LIST_STORAGE_FILE = "Using order list storage file : %1$s";
 }
