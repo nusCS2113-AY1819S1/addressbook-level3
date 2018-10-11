185b33de18d1d36b0160e70bad545c3e874d7779
kianhong95 <kianhong1995@gmail.com>
2018-10-11 18:10:45 +0800

    Added AddEmployeeCommand

---
+++ src/seedu/addressbook/commands/EmployeeAddCommand.java
@@ -0,0 +1,74 @@
+package seedu.addressbook.commands;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.Address;
+import seedu.addressbook.data.person.Email;
+import seedu.addressbook.data.person.Name;
+import seedu.addressbook.data.person.Employee;
+import seedu.addressbook.data.person.Phone;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.data.person.UniqueEmployeeList;
+import seedu.addressbook.data.tag.Tag;
+
+/**
+ * Adds a new employee.
+ */
+
+public class EmployeeAddCommand extends Command{
+
+    public static final String COMMAND_WORD = "addemp";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" + "Adds an employee to the address book. "
+            + "Contact details can be marked private by prepending 'p' to the prefix.\n\t"
+            + "Parameters: NAME [p]p/PHONE [p]e/EMAIL [p]a/ADDRESS  [t/TAG]...\n\t"
+            + "Example: " + COMMAND_WORD
+            + " John Doe p/98765432 e/johnd@gmail.com a/311, Clementi Ave 2, #02-25 t/friends t/owesMoney";
+
+    public static final String MESSAGE_SUCCESS = "New person added: %1$s";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
+
+    private final Employee toAdd;
+
+    /**
+     * Convenience constructor using raw values.
+     *
+     * @throws IllegalValueException if any of the raw values are invalid
+     */
+    public EmployeeAddCommand(String name,
+                      String phone, boolean isPhonePrivate,
+                      String email, boolean isEmailPrivate,
+                      String address, boolean isAddressPrivate,
+                      Set<String> tags) throws IllegalValueException {
+        final Set<Tag> tagSet = new HashSet<>();
+        for (String tagName : tags) {
+            tagSet.add(new Tag(tagName));
+        }
+        this.toAdd = new Employee(
+                new Name(name),
+                new Phone(phone, isPhonePrivate),
+                new Email(email, isEmailPrivate),
+                new Address(address, isAddressPrivate)
+        );
+    }
+
+    public EmployeeAddCommand(Employee toAdd) {
+        this.toAdd = toAdd;
+    }
+
+    public ReadOnlyPerson getPerson() {
+        return toAdd;
+    }
+
+    @Override
+    public CommandResult execute() {
+        try {
+            rms.addEmployee(toAdd);
+            return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
+        } catch (UniqueEmployeeList.DuplicateEmployeeException dpe) {
+            return new CommandResult(MESSAGE_DUPLICATE_PERSON);
+        }
+    }
+}
--- src/seedu/addressbook/commands/EmployeeListCommand.java
+++ src/seedu/addressbook/commands/EmployeeListCommand.java
@@ -5,12 +5,14 @@ import java.util.List;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 
 
-// this class is a copy of List Command with allPersons changed to allEmployees
+/**
+ * List all employees.
+ */
 public class EmployeeListCommand extends Command{
 
     public static final String COMMAND_WORD = "listemp";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" + "Shows details of all employees. \n\t"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" + "List all employees. \n\t"
             + "Parameters: NIL\n\t"
             + "Example: " + COMMAND_WORD;
 
