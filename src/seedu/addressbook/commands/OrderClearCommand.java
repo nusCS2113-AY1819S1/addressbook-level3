2837d3a958a368d268e8b167f2815cf8afdf653b
px1099 <px1099@gmail.com>
2018-10-11 09:23:03 +0800

    add OrderClearCommand class

---
+++ src/seedu/addressbook/commands/OrderClearCommand.java
@@ -0,0 +1,19 @@
+package seedu.addressbook.commands;
+
+/**
+ * Clears the order list.
+ */
+public class OrderClearCommand extends Command {
+
+    public static final String COMMAND_WORD = "clearorder";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" + "Clears order list permanently.\n\t"
+            + "Example: " + COMMAND_WORD;
+
+    public static final String MESSAGE_SUCCESS = "Order list has been cleared!";
+
+    @Override
+    public CommandResult execute() {
+        rms.clearOrderList();
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
