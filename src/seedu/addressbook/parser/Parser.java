e916272693579d851cafe4658d6ea750dde8bac9
Neow Bo Kai <e0176277@u.nus.edu>
2018-10-14 22:01:30 +0800

    Updated according to Codacy: Since: PMD 5.0
    Import statements allow the use of non-fully qualified names. The use of a fully qualified name which is covered by an import statement is redundant. Consider using the non-fully qualified name.

--- src/seedu/addressbook/parser/Parser.java
+++ src/seedu/addressbook/parser/Parser.java
@@ -646,7 +646,7 @@ public class Parser {
         }
 
         try {
-            final int targetIndex = Integer.parseInt(matcher.group("targetIndex").trim());
+            final int targetIndex = parseInt(matcher.group("targetIndex"));
             return new EditExamCommand(targetIndex, changedDetails);
         } catch (NumberFormatException e) {
             return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditExamCommand.MESSAGE_USAGE));
