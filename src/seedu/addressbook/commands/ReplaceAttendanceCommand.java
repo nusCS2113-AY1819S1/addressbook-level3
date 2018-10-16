a814d45e4367b107ea357e3539ffda1b549fe4c2
Neow Bo Kai <e0176277@u.nus.edu>
2018-10-16 17:59:47 +0800

    Added in replaceAttendanceCommand to overwrite current attendance

--- src/seedu/addressbook/commands/ReplaceAttendanceCommand.java
+++ src/seedu/addressbook/commands/ReplaceAttendanceCommand.java
@@ -5,26 +5,26 @@ import seedu.addressbook.data.person.Person;
 import seedu.addressbook.data.person.UniquePersonList;
 
 /**
- *  Marks the date where the student is present.
+ *  Replaces the already marked attendance for the given date.
  */
-public class UpdateAttendanceCommand extends Command {
+public class ReplaceAttendanceCommand extends Command {
 
-    public static final String COMMAND_WORD = "attendance";
+    public static final String COMMAND_WORD = "replaceAtten";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n"
             + "Updates the attendance of a student. \n"
             + "Parameters: indexOfPerson d/dd-mm-yyyy att/attendance \n"
             + "Example: " + COMMAND_WORD + " " + "1 d/29-09-2018 att/1 \n"
             + "To input today's date, input d/0";
 
-    public static final String MESSAGE_SUCCESS = "Attendance updated for: ";
-    public static final String MESSAGE_DUPLICATE_ATTENDANCE = "Attendance has already been taken.\n"
-            + "Please use `replaceAtten` command to overwrite current attendance.";
+    public static final String MESSAGE_SUCCESS = "Attendance replaced for: ";
+    public static final String MESSAGE_NO_DUPLICATE_ATTENDANCE = "Attendance has yet to be taken.\n"
+            + "Please use `attendance` command to add attendance";
 
     private boolean isPresent;
     private String date;
 
     // Constructor
-    public UpdateAttendanceCommand(int targetIndex, String date, boolean isPresent) {
+    public ReplaceAttendanceCommand(int targetIndex, String date, boolean isPresent) {
         super(targetIndex); // super is calling the constructor of the parent function
         this.date = date;
         this.isPresent = isPresent;
@@ -34,9 +34,9 @@ public class UpdateAttendanceCommand extends Command {
     public CommandResult execute() {
         try {
             Person person = addressBook.findPerson(getTargetPerson());
-            boolean duplicateDate = person.updateAttendanceMethod(date, isPresent, true);
-            if (duplicateDate) {
-                return new CommandResult(String.format((MESSAGE_DUPLICATE_ATTENDANCE)));
+            boolean NoDuplicateDate = person.replaceAttendanceMethod(date, isPresent, false);
+            if (NoDuplicateDate) {
+                return new CommandResult(String.format((MESSAGE_NO_DUPLICATE_ATTENDANCE)));
             } else {
                 return new CommandResult(String.format(MESSAGE_SUCCESS) + person.getName());
             }
