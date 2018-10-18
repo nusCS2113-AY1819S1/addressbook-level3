013ff4de22311c087a7b05be3764d75f9b1dac88
Ong Jia Jian <ojayjae.7@gmail.com>
2018-10-18 16:13:49 +0800

    increase login tries to 3 from 1

--- src/seedu/addressbook/login/login.java
+++ src/seedu/addressbook/login/login.java
@@ -1,10 +1,28 @@
 package seedu.addressbook.login;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class login {
-//    private boolean loggedin;
+    private boolean loggedin;
 //    private boolean debug = true;
+    private static int tries = 0;
+
+//    public static String[] getLogin(){
+//        String stdUser;
+//        String stdPass;
+//        stdUser = "Username";
+//        stdPass = "Password";
+//
+//        String username;
+//        String password;
+//
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Username: ");
+//        username = sc.next();
+//        System.out.print("Password: ");
+//        password = sc.next();
+//    }
 
     public static boolean main(){
         String stdUser;
@@ -16,22 +34,24 @@ public class login {
         String password;
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("Username: ");
-        username = sc.next();
-        System.out.print("Password: ");
-        password = sc.next();
+        for(tries=0; tries<3; tries++){
+            System.out.print("Username: ");
+            username = sc.next();
+            System.out.print("Password: ");
+            password = sc.next();
 
-        if(username.equals(stdUser) && password.equals(stdPass)){
-            System.out.println("Welcome to AddressBook3");
-            return true;
-        }else if(WorkWithLoginStorage.compareCredentials(username, password)){
-            System.out.println("2Welcome to AddressBook3");
-            return true;
-        }else{
-            System.out.println("Login failed");
-            return false;
+            if(username.equals(stdUser) && password.equals(stdPass)){
+                System.out.println("Welcome to AddressBook3");
+                return true;
+            }else if(WorkWithLoginStorage.compareCredentials(username, password)){
+                System.out.println("2Welcome to AddressBook3");
+                return true;
+            }else{
+                System.out.println("Incorrect Username/Password! Please try again.");
+            }
         }
-
+        System.out.println("Login failed. Account is now locked.");
+        return false;
 //        System.out.println(username + "   " + password);
     }
 }
