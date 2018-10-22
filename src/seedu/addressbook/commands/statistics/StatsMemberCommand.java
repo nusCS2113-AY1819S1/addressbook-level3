7ac3478a43f468578895d76594d865d346221995
Wei Ming <>
2018-10-22 17:08:28 +0800

    Update statsmember command

--- src/seedu/addressbook/commands/statistics/StatsMemberCommand.java
+++ src/seedu/addressbook/commands/statistics/StatsMemberCommand.java
@@ -1,10 +1,14 @@
 package seedu.addressbook.commands.statistics;
 
 import java.util.List;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import seedu.addressbook.commands.Command;
 import seedu.addressbook.commands.CommandResult;
 import seedu.addressbook.data.member.ReadOnlyMember;
+import seedu.addressbook.data.statistics.MemberDateTable;
 
 /**
  *
@@ -26,11 +30,25 @@ public class StatsMemberCommand extends Command {
     private String getMemberStats() {
         StringBuilder res = new StringBuilder();
         List<ReadOnlyMember> allMembers = rms.getAllMembers().immutableListView();
-        for (ReadOnlyMember member : allMembers) {
 
+        MemberDateTable dateTable = new MemberDateTable();
+
+        for (ReadOnlyMember member : allMembers) {
+            // Replace with member.getDate() during merge
+            Date temp = new Date();
+            // ==========================================
+            dateTable.addData(temp);
         }
         res.append("Number of members: " + allMembers.size());
         appendNewLine(res);
+        appendNewLine(res);
+        res.append("New members this year: " + dateTable.getYearCount(new Date()));
+        appendNewLine(res);
+        appendNewLine(res);
+        res.append("New members this month: " + dateTable.getMonthCount(new Date()));
+        appendNewLine(res);
+        appendNewLine(res);
+        res.append("New members today: " + dateTable.getDayCount(new Date()));
         return res.toString();
     }
 
