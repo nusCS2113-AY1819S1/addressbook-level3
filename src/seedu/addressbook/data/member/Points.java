71cff44d3229fefd87b44316871855a9f49ae709
kangmingtay <kang.ming1996@gmail.com>
2018-10-24 18:02:17 +0800

    add exception to method updatePoints in member class

--- src/seedu/addressbook/data/member/Points.java
+++ src/seedu/addressbook/data/member/Points.java
@@ -1,6 +1,10 @@
 package seedu.addressbook.data.member;
 
+import seedu.addressbook.data.exception.IllegalValueException;
+
 import java.awt.*;
+
+import static seedu.addressbook.common.Messages.MESSAGE_NEGATIVE_POINTS;
 
 public class Points {
 
@@ -13,14 +17,25 @@ public class Points {
     public Points(int points){
         this.value = points;
     }
+
+//    public final String MESSAGE_NEGATIVE_POINTS = "Update points cannot result in negative points.";
     /**
      * Converts the price into points and adds in to the existing points for the member
      * @param price of the order being made
      * @return updated points
      */
     protected Points updatePoints(double price) {
-        this.value = ((int)price) / 10;
-        return this;
+        try {
+            this.value += ((int) price) / 10;
+            if(this.value < 0) {
+                throw new IllegalValueException(MESSAGE_NEGATIVE_POINTS);
+            }
+            return this;
+        } catch (IllegalValueException e) {
+            this.value = 0;
+            return this;
+        }
+
     }
 
     public int getPoints() {
