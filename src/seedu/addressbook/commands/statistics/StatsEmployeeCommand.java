f724925b4926c127577e6bc5174d008bf024d953
Wei Ming <>
2018-10-30 00:21:18 +0800

    Update statistics command for employee

--- src/seedu/addressbook/commands/statistics/StatsEmployeeCommand.java
+++ src/seedu/addressbook/commands/statistics/StatsEmployeeCommand.java
@@ -1,10 +1,14 @@
 package seedu.addressbook.commands.statistics;
 
 import java.util.List;
+import java.util.Set;
 
 import seedu.addressbook.commands.Command;
 import seedu.addressbook.commands.CommandResult;
+import seedu.addressbook.data.employee.Attendance;
 import seedu.addressbook.data.employee.ReadOnlyEmployee;
+import seedu.addressbook.data.employee.Timing;
+import seedu.addressbook.data.employee.UniqueAttendanceList;
 import seedu.addressbook.data.menu.ReadOnlyMenus;
 import seedu.addressbook.data.statistics.AsciiTable;
 
@@ -26,18 +30,31 @@ public class StatsEmployeeCommand extends Command {
     }
 
     private String getEmployeeStats() {
-//        StringBuilder res = new StringBuilder();
-//        List<ReadOnlyEmployee> allEmployees = rms.getAllEmployees().immutableListView();
-//        if (allEmployees.isEmpty())
-//            return "There are no employees in the system.";
-//
-//        String[] headings = new String[]{"1","2","333"};
-//        AsciiTable table = new AsciiTable(headings);
-//        for (ReadOnlyEmployee emp : allEmployees) {
-//            String[] data = new String[]{"1", emp.getName().value, emp.getPosition().value};
-//            table.addRow(data);
-//        }
-//        return table.toString();
-        return "Work In Progress\n";
+        StringBuilder res = new StringBuilder();
+        List<ReadOnlyEmployee> allEmployees = rms.getAllEmployees().immutableListView();
+        UniqueAttendanceList allAttendance = rms.getAllAttendance();
+        if (allEmployees.isEmpty())
+            return "There are no employees in the system.";
+        res.append("Number of employees: " + allEmployees.size() + "\n\n");
+        res.append("Currently on duty employees: ");
+        String[] headings = new String[]{"Name", "Position", "Clocked in"};
+        AsciiTable onduty = new AsciiTable(headings);
+        int count = 0;
+        for (ReadOnlyEmployee emp : allEmployees) {
+            String name = emp.getName().fullName;
+            Attendance attendance = allAttendance.getAttendance(allAttendance.getAttendanceIndex(name));
+            if (attendance.getClockedIn()) {
+                Set<Timing> timing = attendance.getTimings();
+                String[] data = new String[]{name, emp.getPosition().value, ((Timing) timing.toArray()[ timing.size()-1 ]).time};
+                onduty.addRow(data);
+                count++;
+            }
+        }
+        res.append(count + "\n");
+        if (count != 0)
+            res.append(onduty.toString());
+        res.append("\n");
+
+        return res.toString();
     }
 }
