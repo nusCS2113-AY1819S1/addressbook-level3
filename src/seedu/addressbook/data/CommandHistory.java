f937d12225a92cafc4bc9b5c32b5738cfaed8921
Nichpang1 <e0235240@u.nus.edu>
2018-11-02 16:44:28 +0800

    Fixed undo/redo bug

--- src/seedu/addressbook/data/CommandHistory.java
+++ src/seedu/addressbook/data/CommandHistory.java
@@ -1,83 +1,24 @@
 package seedu.addressbook.data;
 
-import seedu.addressbook.data.person.UniquePersonList;
-
 import java.util.ArrayList;
 
 public class CommandHistory {
-    private AddressBook addressBook;
-    private ArrayList<UniquePersonList> addressBookStates = new ArrayList<>();
-    private ArrayList<String> history = new ArrayList<>();
-    private static final int ITERATOROFFSET = 1;
-    private int stateIterator = 0;
+    private ArrayList<String> history;
+//    private AddressBook addressBook;
+//    private ArrayList<UniquePersonList> addressBookStates = new ArrayList<>()
+//    private static final int ITERATOROFFSET = 1;
+//    private int stateIterator = 0;
 
-    public CommandHistory(AddressBook addressBook){
-        this.addressBook = addressBook;
-        saveInitialState();
+    public CommandHistory(){
+        history = new ArrayList<>();
+//        this.addressBook = addressBook;
+//        saveInitialState();
     }
 
-    public static class HistoryOutOfBoundException extends Exception {}
+//    public static class HistoryOutOfBoundException extends Exception {}
+
     public static class EmptyHistoryException extends Exception {}
-    /**
-     * Save initial state of the address book
-     */
-    public void saveInitialState() {
-        this.addressBookStates.add(addressBook.getAllPersons());
-    }
 
-    /**
-     * add the current state of the address book into the history
-     */
-    public void addCurrentState() {
-        moveIteratorForward();
-        addressBookStates.add(addressBook.getAllPersons());
-    }
-
-    public void moveIteratorForward() {
-        stateIterator++;
-    }
-
-    public void moveIteratorBackward() {
-        stateIterator--;
-    }
-
-    /**
-     * truncate old history when new undo-able command is executed after undo-ing
-     */
-    public void truncateOldPath() {
-        addressBookStates.subList(stateIterator + ITERATOROFFSET, addressBookStates.size()).clear();
-    }
-
-    /**
-     * check whether to execute truncateOldPath
-     */
-    public void checkForAction() {
-        if(!nextBoxIsEmpty()) truncateOldPath();
-        addCurrentState();
-    }
-
-    public boolean nextBoxIsEmpty() {
-        if((stateIterator + ITERATOROFFSET) >= addressBookStates.size()) return true;
-        else return false;
-    }
-
-    public void undoLast() throws HistoryOutOfBoundException {
-        moveIteratorBackward();
-        if(stateIterator < 0){
-            moveIteratorForward();
-            throw new HistoryOutOfBoundException();
-        }
-        addressBook.switchAddressBook(addressBookStates.get(stateIterator));
-    }
-
-    public void redoLast() throws HistoryOutOfBoundException {
-        moveIteratorForward();
-        if(stateIterator + ITERATOROFFSET> addressBookStates.size()){
-            moveIteratorBackward();
-            throw new HistoryOutOfBoundException();
-        }
-        addressBook.switchAddressBook(addressBookStates.get(stateIterator));
-    }
     public void addHistory(String Arg) {
         history.add(Arg);
     }
@@ -95,4 +36,64 @@ public class CommandHistory {
         }
         return builder.toString();
     }
+//    /**
+//     * Save initial state of the address book
+//     */
+//    public void saveInitialState() {
+//        this.addressBookStates.add(addressBook.getAllPersons());
+//    }
+//
+//    /**
+//     * add the current state of the address book into the history
+//     */
+//    public void addCurrentState() {
+//        moveIteratorForward();
+//        addressBookStates.add(addressBook.getAllPersons());
+//    }
+//
+//    public void moveIteratorForward() {
+//        stateIterator++;
+//    }
+//
+//    public void moveIteratorBackward() {
+//        stateIterator--;
+//    }
+//
+//    /**
+//     * truncate old history when new undo-able command is executed after undo-ing
+//     */
+//    public void truncateOldPath() {
+//        addressBookStates.subList(stateIterator + ITERATOROFFSET, addressBookStates.size()).clear();
+//    }
+//
+//    /**
+//     * check whether to execute truncateOldPath
+//     */
+//    public void checkForAction() {
+//        if(!nextBoxIsEmpty()) truncateOldPath();
+//        addCurrentState();
+//    }
+//
+//    public boolean nextBoxIsEmpty() {
+//        if((stateIterator + ITERATOROFFSET) >= addressBookStates.size()) return true;
+//        else return false;
+//    }
+//
+//    public void undoLast() throws HistoryOutOfBoundException {
+//        moveIteratorBackward();
+//        if(stateIterator < 0){
+//            moveIteratorForward();
+//            throw new HistoryOutOfBoundException();
+//        }
+//        addressBook.switchAddressBook(addressBookStates.get(stateIterator));
+//    }
+//
+//    public void redoLast() throws HistoryOutOfBoundException {
+//        moveIteratorForward();
+//        if(stateIterator + ITERATOROFFSET> addressBookStates.size()){
+//            moveIteratorBackward();
+//            throw new HistoryOutOfBoundException();
+//        }
+//        addressBook.switchAddressBook(addressBookStates.get(stateIterator));
+//    }
 }
