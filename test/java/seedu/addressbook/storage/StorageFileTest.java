86ed0cea87863ffe32c0162b4c7d222d3855e229
px1099 <px1099@gmail.com>
2018-11-08 18:23:48 +0800

    Update StorageFileTest.java

--- test/java/seedu/addressbook/storage/StorageFileTest.java
+++ test/java/seedu/addressbook/storage/StorageFileTest.java
@@ -4,7 +4,11 @@ import static org.junit.Assert.assertEquals;
 import static seedu.addressbook.util.TestUtil.assertTextFilesEqual;
 
 import java.nio.file.Paths;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
 import org.junit.Rule;
@@ -22,6 +26,17 @@ import seedu.addressbook.data.employee.EmployeePhone;
 import seedu.addressbook.data.employee.EmployeePosition;
 import seedu.addressbook.data.employee.Timing;
 import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.member.Member;
+import seedu.addressbook.data.member.MemberName;
+import seedu.addressbook.data.member.MemberTier;
+import seedu.addressbook.data.member.Points;
+import seedu.addressbook.data.menu.Menu;
+import seedu.addressbook.data.menu.MenuName;
+import seedu.addressbook.data.menu.Price;
+import seedu.addressbook.data.menu.ReadOnlyMenus;
+import seedu.addressbook.data.menu.Type;
+import seedu.addressbook.data.order.Order;
+import seedu.addressbook.data.tag.Tag;
 import seedu.addressbook.storage.StorageFile.StorageOperationException;
 
 public class StorageFileTest {
@@ -103,54 +118,85 @@ public class StorageFileTest {
 
     private Rms getTestRms() throws Exception {
         Rms rms = new Rms();
-        /*
-        rms.addPerson(new Person(new Name("John Doe"),
-                                new Phone("98765432", false),
-                                new Email("johnd@gmail.com", false),
-                                new Address("John street, block 123, #01-01", false),
-                                Collections.emptySet()));
-        rms.addPerson(new Person(new Name("Betsy Crowe"),
-                                new Phone("1234567", true),
-                                new Email("betsycrowe@gmail.com", false),
-                                new Address("Newgate Prison", true),
-                                new HashSet<>(Arrays.asList(new Tag("friend"), new Tag("criminal")))));
-        */
-        generateEmployeeList(rms);
-        generateAttendanceList(rms);
+        generateEmployeeAndAttendanceList(rms);
+        generateMenuAndMemberAndOrderList(rms);
         return rms;
     }
 
     /**
-     * Add a list of employee to the specified rms for testing
+     * Add a list of employee, attendance to the specified rms for testing
      */
-    private void generateEmployeeList(Rms rms) throws Exception {
+    private void generateEmployeeAndAttendanceList(Rms rms) throws Exception {
+        String emp1Name = "Tay";
+        String emp2Name = "Lim";
         Employee emp1 = new Employee(
-                new EmployeeName("Tay"),
+                new EmployeeName(emp1Name),
                 new EmployeePhone("11111111"),
                 new EmployeeEmail("11111111@gmail.com"),
                 new EmployeeAddress("11111111 Street"),
                 new EmployeePosition("Cashier"));
         Employee emp2 = new Employee(
-                new EmployeeName("Lim"),
+                new EmployeeName(emp2Name),
                 new EmployeePhone("22222222"),
                 new EmployeeEmail("22222222@gmail.com"),
                 new EmployeeAddress("22222222 Street"),
                 new EmployeePosition("Cashier"));
+
         rms.addEmployee(emp1);
         rms.addEmployee(emp2);
-    }
 
-    /**
-     * Add a list of attendance to the specified rms for testing
-     */
-    private void generateAttendanceList(Rms rms) {
         Set<Timing> timings = new LinkedHashSet<>();
         timings.add(new Timing("00:00", "11/08/2018", true));
-        Attendance atd1 = new Attendance("Tay", true, timings);
-        Attendance atd2 = new Attendance("Lim", true, timings);
+        Attendance atd1 = new Attendance(emp1Name, true, timings);
+        Attendance atd2 = new Attendance(emp2Name, true, timings);
+
         rms.addAttendance(atd1);
         rms.addAttendance(atd2);
     }
 
+    /**
+     * Add a list of menus, members, orders to the specified rms for testing
+     */
+    private void generateMenuAndMemberAndOrderList(Rms rms) throws Exception {
+        Set<Tag> foodTags = new HashSet<>();
+        foodTags.add(new Tag("best"));
+        Menu menu1 = new Menu(
+                new MenuName("Burger"),
+                new Price("$5.00"),
+                new Type("main"),
+                foodTags);
+        Menu menu2 = new Menu(
+                new MenuName("Fries"),
+                new Price("$2.00"),
+                new Type("sides"),
+                foodTags);
+
+        rms.addMenu(menu1);
+        rms.addMenu(menu2);
+
+        Member member1 = new Member(
+                new MemberName("Ang"),
+                new Points(),
+                new Date(1000),
+                new MemberTier("Bronze"));
+        Member member2 = new Member(
+                new MemberName("Salsabil"),
+                new Points(10000),
+                new Date(1000),
+                new MemberTier("Gold"));
+
+        rms.addMember(member1);
+        rms.addMember(member2);
+
+        Map<ReadOnlyMenus, Integer> foodItems = new HashMap<>();
+        foodItems.put(menu1, 1);
+        foodItems.put(menu2, 2);
+
+        Order order1 = new Order(member1, new Date(1000), 9.0, foodItems, 0);
+        Order order2 = new Order(member2, new Date(1000), 8.0, foodItems, 100);
+
+        rms.addOrder(order1);
+        rms.addOrder(order2);
+    }
 
 }
