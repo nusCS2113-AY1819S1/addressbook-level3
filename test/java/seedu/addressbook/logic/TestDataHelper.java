15659a37127ded281c8afcbfb6618156212f1d32
kianhong95 <kianhong1995@gmail.com>
2018-11-11 21:24:11 +0800

    Fixed CheckStyle erro

--- test/java/seedu/addressbook/logic/TestDataHelper.java
+++ test/java/seedu/addressbook/logic/TestDataHelper.java
@@ -6,7 +6,6 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
