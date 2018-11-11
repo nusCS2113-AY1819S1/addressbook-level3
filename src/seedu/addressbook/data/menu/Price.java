da8496a4279b99c422ad1f4233c0681eced45073
kangmingtay <kang.ming1996@gmail.com>
2018-11-11 12:24:47 +0800

    change message phrasing

--- src/seedu/addressbook/data/menu/Price.java
+++ src/seedu/addressbook/data/menu/Price.java
@@ -11,8 +11,8 @@ public class Price {
 
     public static final String EXAMPLE = "$4.40";
     public static final String MESSAGE_PRICE_CONSTRAINTS = "Price must follow "
-            + "the format $A.BC, where A is a number of 1-3 digits and B and C are 1 digit each, "
-            + "or just $A";
+            + "the format $A.BC or $A, "
+            + "where A is a number of 1-3 digits and B and C are 1 digit each";
 
     public static final String PRICE_VALIDATION_REGEX = "\\$[1-9][0-9]{0,2}(\\.[0-9]{2})?|\\$0{1}\\.[0-9]{2}|\\$0";
     //"^\\$\\d+([.][0-9]+)?$";
