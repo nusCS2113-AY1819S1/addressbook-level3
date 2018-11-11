6f4cc215b7eb53063dd59c4e88d60c367f5bece9
Nichpang1 <e0235240@u.nus.edu>
2018-11-11 15:14:06 +0800

    Revert "Revert "Comments for UndoAbleCommand""
    This reverts commit 315eebad0cd28597b4f2ad073bcdd2cf96fb3e22.

--- src/seedu/addressbook/commands/UndoAbleCommand.java
+++ src/seedu/addressbook/commands/UndoAbleCommand.java
@@ -1,21 +1,40 @@
 package seedu.addressbook.commands;
 
+/**
+ * Represents an executable command.
+ */
 public abstract class UndoAbleCommand extends Command{
 
+    /**
+     * @param targetVisibleIndex last visible listing index of the target person
+     * @param targetVisibleIndex2 last visible listing index of second target person
+     */
     UndoAbleCommand(int targetVisibleIndex, int targetVisibleIndex2){
         super(targetVisibleIndex, targetVisibleIndex2);
     }
 
+    /**
+     * @param targetVisibleIndex last visible listing index of the target person
+     */
     UndoAbleCommand(int targetVisibleIndex){
         super(targetVisibleIndex);
     }
 
     protected UndoAbleCommand(){}
 
+    /**
+     * Execute undo logic which should be implemented in each UndoAbleCommand sub class
+     */
     public abstract void executeUndo() throws Exception;
 
+    /**
+     * Execute redo logic which should be implemented in each UndoAbleCommand sub class
+     */
     public abstract void executeRedo() throws Exception;
 
+    /**
+     * Save the UndoAbleCommand object into the commandStack as well as call saveHistory
+     */
     public void saveUndoableToHistory(String fullCommand){
         saveHistory(fullCommand);
         commandStack.checkForAction(this);
